db3a037ad22b9a0c2eaa4fca51806c5b7b25aee2
Richard Poelderl <richard.poelderl@gmail.com>
2023-02-02 10:04:24 -0300

    fix typo

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,11 @@ import { useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-export default function Providers({ children }: { children?: React.ReactNode }}) {
+export default function Providers({
+  children,
+}: {
+  children?: React.ReactNode
+}) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
